874d8572f555022d291d18db3f57ad5b6be1f85f
KON <KON@123.COM>
2024-11-22 12:09:21 +0800

    重构配置管理，删除config.py，更新导入路径，优化语音识别和人脸识别功能

--- main_gradio.py
+++ main_gradio.py
@@ -2,12 +2,13 @@ import gradio as gr
 import time
 import numpy as np
 import torch
-import moviepy.editor as mp
+import moviepy as mp
 import librosa
 from PIL import Image
 from voice_id import VoiceID  
-from face_recognition import FaceID  
-from database_module6 import Database  
+from face_id import FaceID  
+from database import Database
+from config import Config
 
 img_list = []
 aud_list = []
@@ -97,7 +98,7 @@ with gr.Blocks(fill_height=True) as demo:
     check_local_btn.click(waiting_local,inputs=None,outputs=wait_local).then(check_local, inputs=input_check, outputs=[wait_local,output_check_local_true, output_check_local_false, check_local_btn])
 
     
-demo.launch()
+
 
 
 # main.py
@@ -188,11 +189,12 @@ def recognize(image_frames, audio_frames,voice_id, face_id, database):
     return text_list
 
 #主函数
-def main(video_file, image, audio, tag):
+def main(video_file=None, image=None, audio=None, tag=None):
     global img_list, aud_list, name_list
     # 创建 VoiceID 实例
     # voice_config = {"param1": "value1", "param2": "value2"}  #参数配置,后续添加
-    voice_id = VoiceID()
+    config = Config()
+    voice_id = VoiceID(config)
 
     # 创建 FaceID 实例
     face_config = {"device": "cuda"}  
@@ -258,3 +260,5 @@ def main(video_file, image, audio, tag):
     
     else:
         return None
+
+demo.launch()
